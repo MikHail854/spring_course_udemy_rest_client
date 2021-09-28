89f7305e1a88493a94d676be77cf6817b4b8f9c4
MikHail854 <vip.moneta95@mail.ru>
2021-09-29 01:35:19 +0700

    REST client

---
+++ src/main/java/ru/chigurov/spring/rest/App.java
@@ -0,0 +1,31 @@
+package ru.chigurov.spring.rest;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.chigurov.spring.rest.configuration.MyConfig;
+import ru.chigurov.spring.rest.entity.Employee;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+
+        Communication communication = context.getBean("communication", Communication.class);
+
+        //List<Employee> allEmployees = communication.getAllEmployees();
+        //System.out.println(allEmployees);
+
+        //Employee empById = communication.getEmployee(2);
+        //System.out.println(empById);
+
+        //Employee employee = new Employee("Sveta", "Sokolova", "IT", 1200);
+        //employee.setId(10);
+        //communication.saveEmployee(employee);
+
+        communication.deleteEmployee(10);
+
+    }
+}
